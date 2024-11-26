1bfd101181ff3560dba69b07ba9091d24d133e56
MukBambot118 <134835507+Mukarrambambot@users.noreply.github.com>
2024-11-26 19:17:45 +0000

    app.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
